07472a903e7ca0646b78daa0c25504f2ee28c621
jvbfs <v_menglingkun@saicmotor.com>
2018-06-05 16:32:21 +0800

    merge吧

--- app/src/main/java/com/example/idmin/loadpdfdemo/LazyLoadActivity.java
+++ app/src/main/java/com/example/idmin/loadpdfdemo/LazyLoadActivity.java
@@ -37,7 +37,7 @@ public class LazyLoadActivity extends AppCompatActivity {
 //        fragments.add(new Fragment1());
 //        fragments.add(new Fragment2());
 //        fragments.add(new Fragment3());
-        //哈哈哈哈 必须mearge吧
+        //merge吧
 //        fragments.add(new Fragment4());
 
 
@@ -47,7 +47,6 @@ public class LazyLoadActivity extends AppCompatActivity {
             tabLayout.addTab(tab);
             fragments.add(getfragment(i));
         }
-        //我是aa分支中新加的代码
 
         viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(),fragments,titleArr));
         // 使用 TabLayout 和 ViewPager 相关联
